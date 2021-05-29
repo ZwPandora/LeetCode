53500c4cfc5d525072c45c0c5e7f065601cffc15
Pandora <zw_liaoya@163.com>
2021-05-29 10:53:21 +0800

    S151

---
+++ S151.java
@@ -0,0 +1,25 @@
+public class S151 {
+    public static void main(String[] args) {
+        int[] i = {1, 2, 3, 0, 4};
+        System.out.println(maxProduct(i));
+    }
+
+    public static int maxProduct(int[] nums) {
+        if (nums == null) {
+            return 0;
+        }
+
+        int curMax = 1;
+        int curMin = 1;
+        int result = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            // stroring old max value for min calculation before it's updated
+            int prevMax = curMax;
+            curMax = Math.max(nums[i], Math.max(nums[i] * prevMax, nums[i] * curMin));
+            curMin = Math.min(nums[i], Math.min(nums[i] * prevMax, nums[i] * curMin));
+            result = Math.max(curMax, result);
+        }
+        return result;
+    }
+}
